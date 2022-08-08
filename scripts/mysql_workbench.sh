96df867eb41ed786c16009da7a7a53bdd032788a
Karthik Nair <knairothera@gmail.com>
2022-08-08 17:51:40 +0530

    Update mysql_workbench.sh

--- scripts/mysql_workbench.sh
+++ scripts/mysql_workbench.sh
@@ -1,1 +1,3 @@
-# snap install mysql-workbench-community
+wget https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.30-1ubuntu22.04_amd64.deb
+sudo dpkg -i mysql-workbench-community_8.0.30-1ubuntu22.04_amd64.deb
+rm mysql-workbench-community_8.0.30-1ubuntu22.04_amd64.deb
