520a501ef0034dbf3f023e3c5db14275c0ca181c
Karthik Nair <realkarthiknair@gmail.com>
2023-08-04 20:11:38 +0530

    adhere to pkg_resources deprecation
    ```
    Use of pkg_resources is deprecated in favor of importlib.resources, importlib.metadata and their backports (importlib_resources, importlib_metadata). Some useful APIs are also provided by packaging (e.g. requirements and version parsing). Users should refrain from new usage of pkg_resources and should work to port to importlib-based solutions.
    ```
    See https://setuptools.pypa.io/en/latest/pkg_resources.html

--- scripts/pip_upgrade.py
+++ scripts/pip_upgrade.py
@@ -2,11 +2,15 @@
 
 # Credit : https://stackoverflow.com/a/5839291/12075599
 
-import pkg_resources
-from subprocess import call
+#import pkg_resources
+#from subprocess import call
 
-packages = [dist.project_name for dist in pkg_resources.working_set]
-call("pip install --upgrade " + ' '.join(packages), shell=True)
+#packages = [dist.project_name for dist in pkg_resources.working_set]
+#call("pip install --upgrade " + ' '.join(packages), shell=True)
 
+import importlib.metadata as metadata
+from subprocess import run
 
+packages = [dist.metadata['Name'] for dist in metadata.distributions()]
+run(["pip", "install", "--upgrade"] + packages)
 
